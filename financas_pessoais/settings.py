6b41d214df473243e973623338b7b6b9dfd038e8
Kaike Matos <123702490+K6IK9@users.noreply.github.com>
2025-03-21 16:57:23 -0400

    att contas

--- financas_pessoais/settings.py
+++ financas_pessoais/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
 }
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -112,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "Pt-br"
 
 TIME_ZONE = "UTC"
 
